6f760fe6b965b0f0a995bba87ddbf7d0fa2af65b
tanayshah454 <68338994+tanayshah454@users.noreply.github.com>
2020-09-16 19:08:57 +0530

    Add files via upload

---
+++ battle-craft-1-master/Tanay1/sketch.js
@@ -0,0 +1,79 @@
+var heart1,heart2,player1,player1_img,player2,player1ducked_img,player1ducked,ground,weapon;
+var bg,weapon_img;
+function preload(){
+bg=loadImage("photos/background.png");
+player1_img=loadImage("photos/1noweapons1.png");
+weapon_img=loadImage("photos/weapon2.PNG");
+player2_img=loadImage("photos/2noweapon.png");
+weapon2_img=loadImage("photos/weapon1.png");
+player1ducked_img=loadImage("photos/1noweapons1duck.png");
+}
+function setup() {
+  var canvas = createCanvas(1200,400);
+    ground=createSprite(600,410,1200,20);
+    ground.visible=false;
+    player1=createSprite(100,325,20,20);
+    player1.addImage(player1_img);
+    player1.scale=0.5;
+    weapon=createSprite(player1.x+50,player1.y-35);
+    weapon.addImage(weapon_img);
+    weapon.scale=0.2
+    player2=createSprite(1100,325,20,20);
+    player2.addImage(player2_img);
+    player2.scale=0.5;
+    weapon2=createSprite(player2.x-50,player2.y-35);
+    weapon2.addImage(weapon2_img);
+    weapon2.scale=0.2
+    player1ducked=createSprite(player1.x,player1.y+35,20,20)
+    player1ducked.addImage(player1ducked_img);
+    player1ducked.scale=0.5
+    player1ducked.visible=false;
+}
+function draw() {
+  background(bg);  
+weapon.x=player1.x+50;
+weapon.y=player1.y-35;
+if(keyDown(UP_ARROW)&&player1.y>=327){
+  player1.velocityY=-16;
+}
+player1.velocityY+=0.7;
+player1.collide(ground);
+if(keyDown(DOWN_ARROW)&&player1.y>=327){
+  player1.y=player1.y+70
+  player1.velocityX=0;
+}
+if(keyDown(LEFT_ARROW)){
+  player1.x-=5;
+}
+if(keyDown(RIGHT_ARROW)){
+  player1.x+=5;
+}
+ellipseMode(RADIUS);
+stroke("red");
+strokeWeight(10);
+fill("red");
+ellipse(player1.x,player1.y,30,50)
+weapon2.x=player2.x-50;
+weapon2.y=player2.y-35;
+if(keyDown("w")&&player2.y>=324.75){
+  player2.velocityY=-14.5;
+}
+player2.velocityY+=0.7;
+player2.collide(ground);
+if(keyDown("a")){
+  player2.x-=5;
+}
+if(keyDown("d")){
+  player2.x+=5;
+}
+if(keyDown("s")&&player2.y>=324.75){
+player2.y=player2.y+70
+}
+ellipseMode(RADIUS);
+stroke("red");
+strokeWeight(10);
+fill("red");
+ellipse(player2.x,player2.y,30,50)
+console.log(player2.y)
+drawSprites();
+}
